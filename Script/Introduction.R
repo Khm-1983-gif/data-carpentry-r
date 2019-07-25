972101a3c572f42d897774dd14fd97eda43f754f
Khm-1983-gif <k.almaiman@qu.edu.sa>
2019-07-26 09:34:24 +1000

    updating script

--- Script/Introduction.R
+++ Script/Introduction.R
@@ -5,3 +5,16 @@ length <- 10
 width <- 5
 area <- length * width
 width <- 6
+
+
+?sqrt
+
+rooms <- c(2,1,1,NA,4)
+ mean (rooms)
+mean(rooms, na.rm = TRUE) 
+
+is.na(rooms)
+
+!is.na(rooms)
+## Extract those elements which are not missing values.
+rooms[!is.na(rooms)]

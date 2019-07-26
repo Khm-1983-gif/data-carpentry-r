490d9230e534dbcf2ed6469acab469324572922b
Khm-1983-gif <k.almaiman@qu.edu.sa>
2019-07-26 15:08:29 +1000

    Hi

--- Script/dplyr.R
+++ Script/dplyr.R
@@ -33,3 +33,43 @@ filter( total_meals > 20) %>%
   select(village, total_meals)
 
 
+ interviews %>%
+  group_by(village) %>%
+  summarise(mean_no_mem = mean (no_membrs))
+
+ interviews %>%
+   filter(! is.na (memb_assoc)) %>%
+   group_by(village, memb_assoc) %>%
+   summarise(mean_no_mem = mean (no_membrs),
+             min_membrs = min(no_membrs)) %>% 
+   arrange(desc(min_membrs)) #desc is used to arrange from large to small, if removed it will be the reverse
+ 
+ interviews %>% count(village, sort = TRUE)
+ 
+ interviews %>%
+   group_by(village) %>%
+   summarise(mean_no_mem = mean (no_membrs),
+             min_membrs = min(no_membrs),
+             max_membrs = max(no_membrs),
+             n = n ()) 
+
+ 
+ # if not already included, add month, year, and day columns
+ interviews %>%
+   mutate(month = month(interview_date),
+          day = day(interview_date),
+          year = year(interview_date)) %>%
+   group_by(year, month) %>%
+   summarize(max_no_membrs = max(no_membrs))
+ 
+ 
+# This is helpful for dummy coding
+ interviews_spread <- interviews %>%
+   mutate(wall_type_logical = TRUE) %>%
+   spread(key = respondent_wall_type, value = wall_type_logical, fill = FALSE)
+
+
+          
+interviews_gather <- interviews_spread %>%
+  gather(key = "respondent_wall_type", value = "wall_type_logical",
+         burntbricks:sunbricks)
